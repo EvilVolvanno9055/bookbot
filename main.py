e0aa047b128c05fe0edefe50d986f2ab7fe29af1
EvilVolvanno9055 <danieljamesclarke98@gmail.com>
2025-05-11 19:53:54 +0100

    competed stats and main so they now count words and each letter

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from stats import count_words, letter_count
+from stats import *
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filepath = sys.argv[1]
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -10,7 +18,18 @@ def main(file_path) -> str:
     return text
 
 
+def report(filepath):
+    text = main(filepath)
+    wrd_count = count_words(text)
+    display_dict = letter_count(text)
 
-text = main("books/frankenstein.txt")
-count_words(text)
-print(letter_count(text))
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + filepath)
+    print("----------- Word Count ----------")
+    print(f"Found {wrd_count} total words")
+    print("--------- Character Count -------")
+    display_counts(display_dict)
+    print("============= END ===============")
+
+
+report(filepath)
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def count_words(text):
     word_list = text.split()
     count = len(word_list)
     
-    print(f"{count} words found in the document")
+    return count
 
 def letter_count(text):
     letter_dict = {}
@@ -12,7 +12,18 @@ def letter_count(text):
             letter_dict[letter] += 1
         else:
             letter_dict[letter] = 1
-    
-    print(letter_dict)
+    return letter_dict
 
+def sort_on(dict):
+    return dict["num"]
 
+def display_counts(letter_dict):
+    letter_list = []
+    for char, count in letter_dict.items():
+        count_dict = {"char": char, "num": count}
+        letter_list.append(count_dict)
+    letter_list.sort(reverse=True, key=sort_on)
+    for l in letter_list:
+        char = l["char"]
+        if char.isalpha():
+            print(f"{char}: {l["num"]}")
